a283182c313a5841574cc958443f57d8eea9568d
Ingirorhaun <francescopantusa@gmail.com>
2024-12-17 22:36:25 +0100

    update ProgressChart component to use D3

--- src/charts/ProgressChart.jsx
+++ src/charts/ProgressChart.jsx
@@ -1,53 +1,99 @@
 import { useEffect, useRef } from "react";
-import { ProgressChart } from "./charts";
+import * as d3 from "d3";
 
+/**
+ * ProgressChart Component - Renders a radial progress chart using D3.js
+ * @param {Object} options - Configuration options for the chart (width, height, title)
+ * @param {Number} chartData - The progress value to be displayed (between 0 and 1)
+ * @returns {JSX.Element} - The rendered ProgressChartComponent
+ */
 export default function ProgressChartComponent(props) {
   const { options, chartData } = props;
-  const canvasRef = useRef(null);
+  const ref = useRef(null);
+
+  const margin = {
+    top: 40,
+    left: 30,
+  };
+  const circleRadius = 87;
+  const arcThickness = 10;
+  const progress = chartData;
 
   useEffect(() => {
-    var progressChart = new ProgressChart({
-      canvas: canvasRef.current,
-      seriesName: options.title,
-      padding: {
-        x: 24,
-        y: 24,
-      },
-      labelText: "de votre objectif",
-      lineWidth: 10,
-      data: chartData,
-      colors: ["#FF0000", "#FFF"],
-      font: {
-        size: "26px",
-        family: "Roboto",
-        weight: "700",
-      },
-      title: {
-        align: "left",
-        verticalAlign: "top",
-        fill: "#20253A",
-        font: {
-          weight: "500",
-          size: "15px",
-          family: "Roboto",
-        },
-      },
-      legend: {
-        show: false,
-      },
-      background: {
-        color: "#FBFBFB",
-      },
-    });
-    progressChart.draw();
+    // Remove any existing element
+    d3.select(ref.current).selectAll("*").remove();
+
+    // Create main svg container
+    const svg = d3
+      .select(ref.current)
+      .append("svg")
+      .attr("width", options.width)
+      .attr("height", options.height);
+
+    // Draw background circle
+    svg
+      .append("circle")
+      .attr("class", "bg-circle")
+      .attr("cx", options.width / 2)
+      .attr("cy", options.height / 2)
+      .attr("r", circleRadius);
+
+    // Draw arc around the background circle, counterclockwise starting from top
+    svg
+      .append("path")
+      .attr("class", "progress-arc")
+      .attr(
+        "d",
+        d3
+          .arc()
+          .innerRadius(circleRadius)
+          .outerRadius(circleRadius + arcThickness)
+          .cornerRadius(arcThickness)
+          .startAngle(0)
+          //calculate end angle based on progress
+          .endAngle(progress * -Math.PI * 2)
+      )
+      .attr(
+        "transform",
+        `translate(${options.width / 2}, ${options.height / 2})`
+      );
+
+    // Draw chart title
+    svg
+      .append("text")
+      .attr("class", "chart-title")
+      .attr("x", margin.left)
+      .attr("y", margin.top)
+      .text(options.title);
+
+    // Draw progress text at the center of the chart
+    svg
+      .append("text")
+      .attr("class", "progress-text")
+      .attr("x", options.width / 2)
+      .attr("y", options.height / 2 - 13) //subtract half line height
+      .attr("text-anchor", "middle")
+      .text(`${progress * 100}%`);
+
+    // Add "de votre" text
+    svg
+      .append("text")
+      .attr("class", "progress-subtext")
+      .attr("x", options.width / 2)
+      .attr("y", options.height / 2 + 15) // Position below the percentage
+      .attr("text-anchor", "middle")
+      .text("de votre");
+
+    // Add "objectif" text
+    svg
+      .append("text")
+      .attr("class", "progress-subtext")
+      .attr("x", options.width / 2)
+      .attr("y", options.height / 2 + 40) // Position below "de votre"
+      .attr("text-anchor", "middle")
+      .text("objectif");
+    
   }, [chartData, options.title]);
 
-  return (
-    <canvas
-      id="progressChartCanvas"
-      width={options.width}
-      height={options.height}
-      ref={canvasRef}
-    ></canvas>
-  );
+  return <div id="progressChart" ref={ref}></div>;
 }
